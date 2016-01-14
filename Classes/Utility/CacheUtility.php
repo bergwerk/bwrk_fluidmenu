adfe8f3e65808a1f8193dab0cd71511f86edfa7a
Georg Dümmler <gd@bergwerk.ag>
2016-01-14 13:05:48 +0100

    Revert "new CacheUtility"
    This reverts commit c2c8a78c3710ae4ecb2fe43781bcf1445597e4ac.

--- Classes/Utility/CacheUtility.php
+++ Classes/Utility/CacheUtility.php
@@ -1,123 +1,171 @@
 <?php
 namespace BERGWERK\BwrkFluidmenu\Utility;
 
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2016 Georg Dümmler <gd@bergwerk.ag>
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- *
- * @author	Georg Dümmler <gd@bergwerk.ag>
- * @package	TYPO3
- * @subpackage	bwrk_fluidmenu
- ***************************************************************/
+/* * *************************************************************
+*
+*  Copyright notice
+*
+*  (c) 2014
+*
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+* ************************************************************* */
 
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Frontend\Page\PageRepository;
-
-/**
- * Class CacheUtility
- * @package BERGWERK\BwrkFluidmenu\Utility
- */
 class CacheUtility extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 {
+
+    /*
+    Alle möglichen Dinge, die ich an pi-Base geliebt habe...
+    Jochen, Sören, Martin: Kann das mal einer sauber machen? ;)
+    */
+
     /**
-     * @var \TYPO3\CMS\Extbase\Configuration\ConfigurationManager
+     * _configurationManager
+     *
+     * @var mixed
+     * @access protected
      */
     protected $_configurationManager;
 
     /**
-     * @var string
+     * _extkey
+     *
+     * @var mixed
+     * @access protected
      */
-    protected $_extKey;
+    protected $_extkey;
 
     /**
-     * @var \TYPO3\CMS\Extbase\Mvc\Request
+     * _request
+     *
+     * @var mixed
+     * @access protected
      */
     protected $_request;
 
     /**
-     * CacheUtility constructor.
+     * _cObj
+     *
+     * @var mixed
+     * @access protected
+     */
+    protected $_cObj;
+
+    /**
+     * _settings
+     *
+     * @var mixed
+     * @access protected
+     */
+    protected $_settings;
+
+    /**
+     * _configuration
+     *
+     * @var mixed
+     * @access protected
+     */
+    protected $_configuration;
+
+
+
+
+    /**
+     * __construct function.
+     *
+     * @access public
+     * @return void
      */
     public function __construct()
     {
-        parent::__construct();
-
-        $this->_extKey = GeneralUtility::camelCaseToLowerCaseUnderscored($this->extensionName);
-
-        $this->_request = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Mvc\\Request');
         $objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Object\\ObjectManager');
-
+        $this->_extKey = 'bwrk_markermap';
         $this->_configurationManager = $objectManager->get('TYPO3\\CMS\\Extbase\\Configuration\\ConfigurationManager');
+        $this->_request = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Mvc\\Request');
+        $this->_cObj = $this->_configurationManager->getContentObject();
+        $this->_configuration = $this->_configurationManager->getConfiguration(\TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK);
+        $this->_settings = $this->_configuration['settings'];
+        $this->_gpVars = $this->_request->getArguments();
     }
 
+
+
     /**
-     * @param null $hashVars
-     * @return bool|mixed
+     * getCache function.
+     *
+     * @access public
+     * @param mixed $hashVars (default: null)
+     * @return void
      */
-    public function getCache ($hashVars = null )
+    public function getCache ( $hashVars = null )
     {
         $cacheID = $this->getCacheID(array($hashVars));
-        $data = PageRepository::getHash($cacheID);
-        if(!$data) return false;
-
-        return unserialize($data);
+        $data = \TYPO3\CMS\Frontend\Page\PageRepository::getHash($cacheID);
+        return !$data ? false : unserialize($data);
     }
 
 
     /**
-     * @param null $data
-     * @param null $hashVars
-     * @return null
+     * setCache function.
+     *
+     * @access public
+     * @param mixed $data (default: null)
+     * @param mixed $hashVars (default: null)
+     * @return void
      */
-    public function setCache ($data = null, $hashVars = null )
+    public function setCache ( $data = null, $hashVars = null )
     {
         $lifetime = mktime(23,59,59) + 1 - time();
         $cacheID = $this->getCacheID(array($hashVars));
 
-        PageRepository::storeHash( $cacheID, serialize($data), $this->_extKey.'_cache', $lifetime );
+        \TYPO3\CMS\Frontend\Page\PageRepository::storeHash( $cacheID, serialize($data), $this->extKey.'_cache', $lifetime );
         return $data;
     }
 
+
     /**
-     * @param null $hashVars
-     * @return string
+     * getCacheID function.
+     *
+     * @access private
+     * @param mixed $hashVars (default: null)
+     * @return void
      */
-    private function getCacheID ($hashVars = null )
+    private function getCacheID ( $hashVars = null )
     {
         $additionalHashVars = array(
+            // Auf Seiten-ID beschränken
             'pid'       => $GLOBALS['TSFE']->id,
+            // Auf Sprache beschränken
             'lang'      => $GLOBALS['TSFE']->sys_language_uid,
-            'uid'       => $this->_configurationManager->getContentObject()->data['uid']
+            // Auf UID dieses Content-Elementes (PlugIns) beschränken
+            'uid'       => $this->_cObj->data['uid']
         );
 
-        if(!is_null($GLOBALS['TSFE']->fe_user->user))
-        {
-            $additionalHashVars[] = $GLOBALS['TSFE']->fe_user->user['ses_id'];
-        }
+        // Vielleicht noch alle Get/Post-Variablen berücksichtigen?
+        $additionalHashVars = array_merge($additionalHashVars, $this->_gpVars);
 
-        $additionalHashVars = array_merge($additionalHashVars, $this->_request->getArguments());
-
+        // Und dann noch die übergebenen Variablen?
         $hashVars = array_merge($additionalHashVars, $hashVars);
 
+        // Einen String aus allem generieren
         $hashString = join('|',array_values($hashVars)).join('|', array_keys($hashVars));
 
+        // ... und als kurzen, unique md5 zurückgeben
         return md5($hashString);
     }
 
