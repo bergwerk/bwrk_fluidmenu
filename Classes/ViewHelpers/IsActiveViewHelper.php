2c3d7f183c4d452c0602987b387219f9d6a2ec66
Marvin Huebner <mjh@bergwerk.ag>
2016-05-03 13:44:21 +0200

    add class parameter to IsActiveViewhelper

--- Classes/ViewHelpers/IsActiveViewHelper.php
+++ Classes/ViewHelpers/IsActiveViewHelper.php
@@ -1,32 +1,32 @@
 <?php
 namespace BERGWERK\BwrkFluidmenu\ViewHelpers;
 
-/***************************************************************
- *  Copyright notice
- *
- *  (c) 2016 Georg Dümmler <gd@bergwerk.ag>
- *  All rights reserved
- *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- *
- * @author	Georg Dümmler <gd@bergwerk.ag>
- * @package	TYPO3
- * @subpackage	bwrk_fluidmenu
- ***************************************************************/
+    /***************************************************************
+     *  Copyright notice
+     *
+     *  (c) 2016 Georg Dümmler <gd@bergwerk.ag>
+     *  All rights reserved
+     *
+     *  This script is part of the TYPO3 project. The TYPO3 project is
+     *  free software; you can redistribute it and/or modify
+     *  it under the terms of the GNU General Public License as published by
+     *  the Free Software Foundation; either version 2 of the License, or
+     *  (at your option) any later version.
+     *
+     *  The GNU General Public License can be found at
+     *  http://www.gnu.org/copyleft/gpl.html.
+     *
+     *  This script is distributed in the hope that it will be useful,
+     *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+     *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     *  GNU General Public License for more details.
+     *
+     *  This copyright notice MUST APPEAR in all copies of the script!
+     *
+     * @author	Georg Dümmler <gd@bergwerk.ag>
+     * @package	TYPO3
+     * @subpackage	bwrk_fluidmenu
+     ***************************************************************/
 
 /**
  * Class IsActiveViewHelper
@@ -37,13 +37,14 @@ class IsActiveViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHe
     /**
      * @param int $number
      * @param array $array
+     * @param string $class
      * @return string
      */
-    public function render($number, $array)
+    public function render($number, $array, $class = 'active')
     {
         if(in_array($number, $array))
         {
-            return 'active';
+            return $class;
         }
         return '';
     }
